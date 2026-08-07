b862e86900ce57ddf6b11409120ca46060a719d0
langhakka <langhakka9527@gmail.com>
2026-08-07 14:33:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except ImportError:
 # 配置区域
 # ==============================================================================
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=65bd476a-deb6-4585-87fb-7adbe34809e8",
+    "https://host2play.gratis/server/renew?i=c06aed2b-3d99-4dfc-b7d2-61ed8f892620",
     # 添加更多链接
 ]
 
